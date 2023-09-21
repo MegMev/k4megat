aa51ce482bbacdca4e600c877b28e0ed8a15ab37
Yong Zhou <zyong06@gmail.com>
2023-09-21 15:41:12 +0800

    add: exportHits_tpc.C to output self-stand flat tree

---
+++ digi/options/simple_digi.py
@@ -0,0 +1,99 @@
+from Gaudi.Configuration import *
+
+# ApplicationMgr
+from Configurables import ApplicationMgr
+appMgr = ApplicationMgr(
+                EvtSel = 'NONE',
+                EvtMax   = -1,
+                OutputLevel=INFO
+               )
+
+################################# Servicec ########################################
+
+# Geometry service
+from Configurables import MegatGeoSvc as GeoSvc
+from os import environ, path
+detector_path = environ.get("MEGAT_ROOT", "")
+geoSvc = GeoSvc("MegatGeoSvc",
+                buildType="BUILD_SIMU",
+                detectors=[path.join(detector_path, 'geometry/compact/Megat.xml'),
+                           path.join(detector_path, 'geometry/compact/TPC_readout.xml')],
+                OutputLevel = WARNING)
+appMgr.ExtSvc += [geoSvc]
+
+# Data service
+from Configurables import k4LegacyDataSvc
+dataSvc = k4LegacyDataSvc("EventDataSvc")
+dataSvc.input = "megat.gaudi.root"
+appMgr.ExtSvc += [dataSvc]
+
+# Rndm service (use G4 default engine)
+from Configurables import HepRndm__Engine_CLHEP__HepJamesRandom_
+rdmEngine = HepRndm__Engine_CLHEP__HepJamesRandom_("RndmGenSvc.Engine")
+rdmEngine.SetSingleton = True
+rdmEngine.Seeds = [5685]
+
+from Configurables import RndmGenSvc
+rdmSvc = RndmGenSvc("RndmGenSvc")
+rdmSvc.Engine = rdmEngine.name()
+appMgr.ExtSvc += [rdmEngine, rdmSvc]
+
+################################# Algorithms ########################################
+
+# Fetch the collection into TES
+from Configurables import PodioLegacyInput
+inputAlg = PodioLegacyInput()
+inputAlg.collections = ["TpcSimHits", "CaloSimHits"]
+appMgr.TopAlg += [inputAlg]
+
+# 1. Electron drift to anode surface
+from Configurables import TpcDriftAlg
+driftAlg = TpcDriftAlg("TpcDriftAlg")
+driftAlg.inHits.Path = "TpcSimHits"
+driftAlg.outHits.Path = "TpcDriftHits"
+# driftAlg.maxHits = 500
+# driftAlg.usePoisson= True
+# driftAlg.wvalue= 25
+# driftAlg.trans_diffusion_const = 200
+# driftAlg.long_diffusion_const = 250
+# driftAlg.drift_velocity = 6
+# driftAlg.attach_factor = 0.1
+appMgr.TopAlg += [driftAlg]
+
+# 2. Readout segmentation
+from Configurables import TpcSegmentAlg
+tpcSegAlg = TpcSegmentAlg("TpcSegAlg")
+tpcSegAlg.inHits.Path = "TpcDriftHits"
+tpcSegAlg.outHits.Path = "TpcSegHits"
+tpcSegAlg.readoutName = "TpcStripHits"
+appMgr.TopAlg += [tpcSegAlg]
+
+# 3. Simple smear (add a fixed-width Gaussian noise)
+from Configurables import TpcSimpleSmearAlg
+tpcSmearAlg = TpcSimpleSmearAlg("TpcSmearAlg")
+tpcSmearAlg.inHits.Path = "TpcSegHits"
+tpcSmearAlg.outHits.Path = "TpcHits"
+tpcSmearAlg.energy_sigma = 10 # eV
+tpcSmearAlg.time_sigma = 100 # ps
+appMgr.TopAlg += [tpcSmearAlg]
+
+# 4. CZT calo smearing (fixed-with gassian to edep)
+from Configurables import CaloSimpleSmearAlg
+caloSmearAlg = CaloSimpleSmearAlg("CaloSmearAlg")
+caloSmearAlg.inHits.Path = "CaloSimHits"
+caloSmearAlg.outHits.Path = "CaloHits"
+caloSmearAlg.energy_sigma = 60 # keV
+appMgr.TopAlg += [caloSmearAlg]
+
+################################# Output ########################################
+
+# Select & Write the collections to disk ROOT file
+from Configurables import PodioLegacyOutput
+outAlg = PodioLegacyOutput('outAlg')
+outAlg.filename = 'simple_digi.root'
+outAlg.outputCommands = ['drop *',
+                         'keep TpcHits',
+                         'keep CaloHits'
+                         ]
+appMgr.TopAlg += [outAlg]
+
---
+++ digi/scripts/exportHits_tpc.C
@@ -0,0 +1,49 @@
+void exportHits_tpc( string infile = "../options/simple_digi.root", string outfile = "flat_tpc_hits.root", int idx = -1,
+                     string br_name = "TpcHits", string ro_name = "TpcStripHits" ) {
+
+  // activate megat library
+  LoadMegat();
+
+  // load geometry
+  using namespace megat::utility;
+
+  // string xmlGeometryPath = fmt::format( "{}/geometry/compact/Megat.xml", std::getenv( "MEGAT_ROOT" ) );
+  string xmlGeometryPath = std::getenv( "MEGAT_ROOT" ) + std::string( "/geometry/compact/Megat.xml" );
+  string xmlTpc          = std::getenv( "MEGAT_ROOT" ) + std::string( "/geometry/compact/TPC_readout.xml" );
+  loadGeometry( { xmlGeometryPath, xmlTpc }, ro_name );
+
+  // get converter
+  IdConverter idConv( ro_name );
+  auto        decoder = idConv.decoder( "TPC" );
+
+  // prepare some processors
+  using ROOT::RVecD;
+  using namespace ROOT::VecOps;
+  ROOT::EnableImplicitMT();
+
+  // algorithm to get position of hited cell
+  megat::Tracker::cell_pos cell_pos( ro_name );
+
+  // algorithm to normalize u,v planes in strip readout
+  auto norm_pos = [&]( const RVec<RVecD>& poss, const RVec<CellID>& ids ) {
+    auto mapper = [&]( const RVecD& pos, const CellID& id ) {
+      return ( decoder->get( id, "layer" ) == 1 ) ? Take( pos, { 1, 0, 2 } ) : pos;
+    };
+    return Map( poss, ids, mapper );
+  };
+
+  auto get_layer = [&]( const RVec<CellID>& ids ) {
+    return Map( ids, [&]( const CellID& id ) { return decoder->get( id, "layer" ); } );
+  };
+
+  // real job
+  ROOT::RDataFrame df( "events", infile );
+  auto             dfout = df.Define( "id", megat::Tracker::hit_id, { br_name } )
+                   .Define( "energy", megat::Tracker::hit_e, { br_name } )
+                   .Define( "time", megat::Tracker::hit_t, { br_name } )
+                   .Define( "layer", get_layer, { "id" } )
+                   .Define( "cell_pos", cell_pos, { br_name } )
+                   .Define( "norm_pos", norm_pos, { "cell_pos", "id" } )
+                   .Define( "position", VecAt<double>( 0 ), { "norm_pos" } )
+                   .Snapshot( "flat_events", outfile, { "layer", "position", "energy", "time" } );
+}
